fe6aa2a4371bd4dec00b900491fd97f8b5ad7865
danembb <dbrophy720@gmail.com>
2021-08-06 11:12:03 -0600

    Begin model test creation for file

--- spec/features/gardens/show_spec.rb
+++ spec/features/gardens/show_spec.rb
@@ -1,18 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'gardens show page' do
-  before :each do
-    @garden1 = Garden.create!(name: "Stardew Gardens", organic: true)
-
-    @plot1 = @garden1.plots.create!(number: 4, size: "Small", direction: "West")
-    @plot2 = @garden1.plots.create!(number: 5, size: "Tiny", direction: "East")
-
-    @plant1 = @plot1.plants.create!(name: "Cauliflower", description: "Why is this worth so much??", days_to_harvest: 7)
-    @plant2 = @plot1.plants.create!(name: "Coffee Beans", description: "Is someone gonna help me harvest all these coffee beans??", days_to_harvest: 13)
-    @plant3 = @plot2.plants.create!(name: "Starfruit", description: "WERE RICH!!!", days_to_harvest: 9)
-    @plant4 = @plot2.plants.create!(name: "Cauliflower", description: "This is still worth a ridiculous amount", days_to_harvest: 7)
-    @plant5 = @plot2.plants.create!(name: "Ancient Fruit", description: "???", days_to_harvest: 101)
-  end
 
   # User Story 3, Garden's Plants
   # As a visitor
@@ -22,9 +10,17 @@ RSpec.describe 'gardens show page' do
   # And I see that this list only includes plants that take less than 100 days to harvest
   describe 'when i visit a gardens show page'
     it 'can see a unique list of all my plots plants of plants less than 100 days to harvest' do
-      visit "/gardens/#{@garden1.id}"
+      garden1 = Garden.create!(name: "Stardew Gardens", organic: true)
 
-      save_and_open_page
+      plot1 = garden1.plots.create!(number: 4, size: "Small", direction: "West")
+      plot2 = garden1.plots.create!(number: 5, size: "Tiny", direction: "East")
+
+      plant1 = plot1.plants.create!(name: "Cauliflower", description: "Why is this worth so much??", days_to_harvest: 7)
+      plant2 = plot1.plants.create!(name: "Coffee Beans", description: "Is someone gonna help me harvest all these coffee beans??", days_to_harvest: 13)
+      plant3 = plot2.plants.create!(name: "Starfruit", description: "WERE RICH!!!", days_to_harvest: 9)
+      plant4 = plot2.plants.create!(name: "Cauliflower", description: "This is still worth a ridiculous amount", days_to_harvest: 7)
+      plant5 = plot2.plants.create!(name: "Ancient Fruit", description: "???", days_to_harvest: 101)
+
       expect(page).to have_content(@plant1.name)
       expect(page).to have_content(@plant2.name)
       expect(page).to have_content(@plant3.name)
--- spec/models/garden_spec.rb
+++ spec/models/garden_spec.rb
@@ -4,4 +4,23 @@ RSpec.describe Garden do
   describe 'relationships' do
     it { should have_many(:plots) }
   end
+
+  describe '#instance methods' do
+    it 'can see only distinct plants' do
+      garden1 = Garden.create!(name: "Stardew Gardens", organic: true)
+
+      plot1 = garden1.plots.create!(number: 4, size: "Small", direction: "West")
+      plot2 = garden1.plots.create!(number: 5, size: "Tiny", direction: "East")
+
+      plant1 = plot1.plants.create!(name: "Cauliflower", description: "Why is this worth so much??", days_to_harvest: 7)
+      plant2 = plot1.plants.create!(name: "Coffee Beans", description: "Is someone gonna help me harvest all these coffee beans??", days_to_harvest: 13)
+      plant3 = plot2.plants.create!(name: "Starfruit", description: "WERE RICH!!!", days_to_harvest: 9)
+      plant4 = plot2.plants.create!(name: "Cauliflower", description: "This is still worth a ridiculous amount", days_to_harvest: 7)
+      plant5 = plot2.plants.create!(name: "Ancient Fruit", description: "???", days_to_harvest: 101)
+
+      expect(garden1.all_distinct_plants.first.name).to eq("Cauliflower")
+      expect(garden1.all_distinct_plants.last.name).to eq("Starfruit")
+      expect(garden1.all_distinct_plants.count).to eq(3)
+    end
+  end
 end
