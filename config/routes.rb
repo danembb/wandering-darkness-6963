5de7c1d645770b241866d8469ddbaa37251416b4
danembb <dbrophy720@gmail.com>
2021-08-06 09:49:11 -0600

    Create gardens show spec, route, view and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get 'gardens/:id', to: 'gardens#show'
+
   get 'plots', to: 'plots#index'
   delete '/plots/', to: 'plot/plants#destroy'
 end
---
+++ spec/features/gardens/show_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'gardens show page' do
+  before :each do
+    @garden1 = Garden.create!(name: "Stardew Gardens", organic: true)
+
+    @plot1 = @garden1.plots.create!(number: 4, size: "Small", direction: "West")
+    @plot2 = @garden1.plots.create!(number: 5, size: "Tiny", direction: "East")
+
+    @plant1 = @plot1.plants.create!(name: "Cauliflower", description: "Why is this worth so much??", days_to_harvest: 7)
+    @plant2 = @plot1.plants.create!(name: "Coffee Beans", description: "Is someone gonna help me harvest all these coffee beans??", days_to_harvest: 13)
+    @plant3 = @plot2.plants.create!(name: "Starfruit", description: "WERE RICH!!!", days_to_harvest: 9)
+    @plant3 = @plot2.plants.create!(name: "Cauliflower", description: "This is still worth a ridiculous amount", days_to_harvest: 7)
+  end
+
+  # User Story 3, Garden's Plants
+  # As a visitor
+  # When I visit a garden's show page
+  # Then I see a list of plants that are included in that garden's plots
+  # And I see that this list is unique (no duplicate plants)
+  # And I see that this list only includes plants that take less than 100 days to harvest
+  describe 'when i visit a gardens show page'
+    it 'can see a unique list of all my plots plants of plants less than 100 days to harvest' do
+      visit "/gardens/#{@garden1.id}"
+
+
+      expect(page).to have_content(@plant1.name)
+      expect(page).to have_content(@plant2.name)
+      expect(page).to have_content(@plant3.name)
+    end
+end
