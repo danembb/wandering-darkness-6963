e3af1c210d64a5cf5468ef551f23915ebe87cd5d
danembb <dbrophy720@gmail.com>
2021-08-06 08:58:13 -0600

    Create view and ensure passing tests for user story 1 completion

--- spec/features/plot/index_spec.rb
+++ spec/features/plot/index_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe 'plot index page' do
   before :each do
     @garden1 = Garden.create!(name: "Stardew Gardens", organic: true)
 
-    @plot1 = @garden1.plots.create!(number: 10, size: "Small", direction: "West")
-    @plot2 = @garden1.plots.create!(number: 8, size: "Tiny", direction: "East")
+    @plot1 = @garden1.plots.create!(number: 4, size: "Small", direction: "West")
+    @plot2 = @garden1.plots.create!(number: 5, size: "Tiny", direction: "East")
 
     @plant1 = @plot1.plants.create!(name: "Cauliflower", description: "Why is this worth so much??", days_to_harvest: 7)
     @plant2 = @plot1.plants.create!(name: "Coffee Beans", description: "Is someone gonna help me harvest all these coffee beans??", days_to_harvest: 13)
@@ -14,18 +14,18 @@ RSpec.describe 'plot index page' do
 
   # User Story 1, Plots Index Page
   # As a visitor
-  # When I visit the plots index page ('/plots')
-  # I see a list of all plot numbers
-  # And under each plot number I see names of all that plot's plants
+  # When I visit the plots index page ('/plots') x
+  # I see a list of all plot numbers x
+  # And under each plot number I see names of all that plot's plants x
   describe 'when i visit the plots index page' do
     it 'can see all plot numbers and names of all a plots plants' do
       visit '/plots/'
 
       expect(page).to have_content(@plot1.number)
-      expect(page).to_not have_content(@plot2.number)
+      expect(page).to have_content(@plot2.number)
       expect(page).to have_content(@plant1.name)
       expect(page).to have_content(@plant2.name)
-      expect(page).to_not have_content(@plant3.name)
+      expect(page).to have_content(@plant3.name)
     end
   end
 end
